b966f9827fe0e3267a4b0d788b36b216bab6ae58
Hernan Rinaldi <hernanjavierrinaldi@gmail.com>
2023-11-21 19:57:33 -0300

    add/foldersStylesForHomeAndRegister

---
+++ src/Presentation/views/register/Styles.tsx
@@ -0,0 +1,63 @@
+/*============================ STYLES =============================*/
+
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "#fff",
+    },
+    imageBackground: {
+      width: "100%",
+      height: "100%",
+      bottom: "30%",
+    },
+    form: {
+      width: "100%",
+      height: "70%",
+      backgroundColor: "white",
+      position: "absolute",
+      bottom: 0,
+      borderTopLeftRadius: 30,
+      borderTopRightRadius: 30,
+    },
+    formTextInput: {
+      flex: 1,
+      borderBottomWidth: 1,
+      borderBottomColor: "#AAAAAA",
+    },
+    formInput: {
+      flexDirection: "row",
+      marginTop: 40,
+      paddingLeft: 20,
+      paddingRight: 20,
+      marginBottom: -7,
+    },
+    formIcon: {
+      width: 25,
+      height: 25,
+      marginRight: 15,
+    },
+    formRegister: {
+      flexDirection: "row",
+      justifyContent: "center",
+      marginTop: 20,
+    },
+    logoContainer: {
+      position: "absolute",
+      alignSelf: "center",
+      top: "5%",
+    },
+    logoImage: {
+      width: 100,
+      height: 100,
+      alignSelf: "center",
+    },
+    logoText: {
+      alignSelf: "center",
+      top: 10,
+      color: "white",
+    },
+  });
+
+  export default styles;
