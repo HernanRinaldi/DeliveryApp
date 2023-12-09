35c6572a6ae079cfee2e32b80e08fbe039fcd072
Hernan Rinaldi <hernanjavierrinaldi@gmail.com>
2023-12-08 23:22:13 -0300

    add/validateFuntionForm

--- src/Presentation/views/register/ViewModel.tsx
+++ src/Presentation/views/register/ViewModel.tsx
@@ -2,6 +2,8 @@ import  { useState } from "react";
 import { RegisterAuthUseCase } from "../../../Domain/useCases/auth/RegisterAuth";
 
 const RegisterViewModel = () => {
+  const [validateForm, setValidateForm] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
   const [values, setValues] = useState({
     name: "",
     lastName: "",
@@ -22,10 +24,38 @@ const RegisterViewModel = () => {
    
   }
 
+  const isValidForm = ():boolean => {
+    if( values.name === '' ){
+      setErrorMessage('Ingresa tu nombre');
+    }
+    if( values.lastName === '' ){
+      setErrorMessage('Ingresa tu apellido');
+    }
+    if( values.email === '' ){
+      setErrorMessage('Ingresa tu correo electronico');
+    }
+    if( values.telephone === '' ){
+      setErrorMessage('Ingresa tu telefono');
+    }
+    if( values.password === '' ){
+      setErrorMessage('Ingresa tu contraseña');
+    }
+    if( values.confirm_password === '' ){
+      setErrorMessage('Confirma tu contraseña');
+    }
+    if( values.password !== values.confirm_password ){
+      setErrorMessage('Confirma tu contraseña correctamente');
+    }
+    
+    return true;
+  }
+
   return {
     ...values,
     onChange,
-    register
+    register,
+    isValidForm,
+    errorMessage
   };
 };
 
