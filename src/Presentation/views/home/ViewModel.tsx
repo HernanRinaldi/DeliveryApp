09887fe7117748171cfd3ae56b0df561e55b83c0
Hernan Rinaldi <hernanjavierrinaldi@gmail.com>
2023-12-29 22:51:18 -0300

    fixViewModelHome

--- src/Presentation/views/home/ViewModel.tsx
+++ src/Presentation/views/home/ViewModel.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { LoginAuthUseCase } from "../../../Domain/useCases/auth/LoginAuth";
 
 const HomeViewModel = () => {
-  const [errorMessage, setErrorMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState("");
   const [values, setValues] = useState({
     email: "",
     password: "",
@@ -11,35 +11,32 @@ const HomeViewModel = () => {
   const onChange = (property: string, value: any) => {
     setValues({ ...values, [property]: value });
   };
-  
-  
-  const login =  async( email:string, passport:string )=>{
-    if(isValidForm()){
+
+  const login = async (email: string, passport: string) => {
+    if (isValidForm()) {
       const resp = await LoginAuthUseCase(email, passport);
       console.log(JSON.stringify(resp));
-
     }
-  }
-  
-  const isValidForm = (): boolean =>{
-    if (values.email == '') {
-      setErrorMessage('Agrege un email')
+  };
+
+  const isValidForm = (): boolean => {
+    if (values.email == "") {
+      setErrorMessage("Agrege un email");
       return false;
     }
-    if (values.password == '') {
-      setErrorMessage('Agrege un password')
+    if (values.password == "") {
+      setErrorMessage("Agrege un password");
       return false;
     }
     return true;
-  }
-  
+  };
 
   return {
     ...values,
     onChange,
     login,
     isValidForm,
-    errorMessage
+    errorMessage,
   };
 };
 
